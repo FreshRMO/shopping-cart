34654a3f2efb135327f33c7a22a08d6cbce727fe
Ben Strater-Vidal <ben.strater-vidal@gmail.com>
2019-02-17 15:07:21 -0700

    fixed quantity issue, working fine now

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 class AddItem extends Component {
   constructor(props) {
     super(props)
-    this.state = {quantity: 1,productID: this.props.products[0].id}
+    this.state = {quantity: 1, productID: this.props.products[0].id}
   }
 
   onSubmit = (e) => {
@@ -23,20 +23,20 @@ class AddItem extends Component {
 
   render() {
     return (
-      <form onSubmit={this.onSubmit}>
         <div className="form-group">
-          <label htmlFor="quantity">Quantity</label>
-          <input type="number" className="form-control" id="quantity" value={this.state.quantity} onChange={this.onChange}/>
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="quantity0">Quantity</label>
+          <input type="number" min='1' name='quantity' className="form-control" id="quantity0" value={this.state.quantity} onChange={this.onChange}/>
 
-          <label htmlFor="product">Product</label>
-          <select id="product" className="form-control" name="productID" onChange={this.onChange}>
+          <label htmlFor="product0">Product</label>
+          <select id="product0" className="form-control" name="productID" onChange={this.onChange}>
             <option>Choose...</option>
             {this.props.products.map((product, i) => <option key={i} value={product.id}>{product.name}</option>)}
           </select>
 
           <button type="submit" className="btn btn-primary">Submit</button>
+          </form>
         </div>
-      </form>
     )
   }
 }
